c2828de3ed35327b0143b681f92b9c1d6357cd49
Scyfi <Scyfii@gmail.com>
2011-05-12 17:32:59 -0700

    second commit

---
+++ RemoteDet/src/org/scyfi/RemoteDet/RDListener.java
@@ -0,0 +1,41 @@
+package org.scyfi.RemoteDet;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class RDListener extends PlayerListener{
+	
+	public RemoteDet plugin;
+	public RDListener(RemoteDet instance){
+		plugin = instance;
+	}
+	
+	public void onPlayerInteract(PlayerInteractEvent event){
+		
+		Player player = event.getPlayer();
+		Block block = event.getClickedBlock();
+		
+		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+			if(block.getType() == Material.TNT){
+				if(player.getItemInHand().getType() == Material.STONE_BUTTON){
+					if(plugin.isRemoteEnabled(player)){
+						plugin.armRemote(player, block);
+					}
+					event.setCancelled(true);
+				}
+			}
+		}
+		else if(event.getAction().equals(Action.LEFT_CLICK_AIR)){
+			if(player.getItemInHand().getType() == Material.STONE_BUTTON){
+				if (plugin.isRemoteArmed(player)){
+					plugin.detonateTNT(player);
+				}
+			}
+		}
+	}
+
+}
---
+++ RemoteDet/src/org/scyfi/RemoteDet/RemoteDet.java
@@ -0,0 +1,98 @@
+package org.scyfi.RemoteDet;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
+import net.minecraft.server.EntityTNTPrimed;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.entity.CraftTNTPrimed;
+import org.bukkit.event.Event;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RemoteDet extends JavaPlugin{
+	
+	public final Logger log = Logger.getLogger("Minecraft");
+	public final String pluginName = "Remote Det";
+	
+	private RDListener playerListener = new RDListener(this);
+	CraftTNTPrimed tntp;
+	
+	public final LinkedList<Player> remoteEnabled = new LinkedList<Player>();
+	public HashMap<Player, Block> armedRemote = new HashMap<Player, Block>();
+	
+	public void onEnable(){
+		log.info(pluginName + " - Version " + this.getDescription().getVersion() + " Enabled");
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
+		
+	}
+	
+	public void onDisable(){
+		log.info(pluginName + " - Version " + this.getDescription().getVersion() + " has been disabled.");
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+			if(!(sender instanceof Player)) {
+				return false;
+			}
+			
+			Player player = (Player) sender;
+			if (command.getName().equalsIgnoreCase("rd")){
+				toggleRemote(player);
+				return true;
+			}	
+			return false;
+	}
+	
+	public void toggleRemote(Player player){
+		if(remoteEnabled.contains(player)){
+			remoteEnabled.remove(player);
+			player.sendMessage(ChatColor.YELLOW + "Remote Detonator disabled!");
+		} else {
+			remoteEnabled.add(player);
+			player.sendMessage(ChatColor.YELLOW + "Remote Detonator enabled!");
+		}
+	}
+	
+	public boolean isRemoteEnabled(Player player){
+		return remoteEnabled.contains(player);
+	}
+	
+	public void armRemote(Player player, Block block){
+			armedRemote.put(player, block);
+			player.sendMessage(ChatColor.RED + "Remote is ARMED!!");
+	}
+	
+	public boolean isRemoteArmed(Player player){
+		if(armedRemote.containsKey(player)){
+			return true;
+		}
+		return false;
+	}
+	
+	//Credit to toasterktn and his source code for this. 
+	public void detonateTNT(Player player) {
+		final Block b;
+		b = armedRemote.get(player);
+		Server server = this.getServer();
+		CraftWorld cWorld = (CraftWorld) b.getWorld();
+		EntityTNTPrimed tnt = new EntityTNTPrimed(cWorld.getHandle(), b.getLocation().getX(), b.getLocation().getY(), b.getLocation().getZ());
+		tnt.setPositionRotation(b.getLocation().getBlockX() + 0.5, b.getLocation().getBlockY(), b.getLocation().getBlockZ() + 0.5, 0, 0);
+		cWorld.getHandle().addEntity(tnt);
+		tntp = new CraftTNTPrimed((CraftServer) server, tnt);
+		b.setType(Material.AIR);
+		armedRemote.remove(player);
+	}
+}
